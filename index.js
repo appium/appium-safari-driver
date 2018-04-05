bad233213156839cbaf8c1f28e4903a170183f08
Jonathan Lipps <jlipps@gmail.com>
2018-04-05 18:32:50 +0100

    allow running of driver on its own

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 import SafariDriver from './lib/driver';
 import { startServer } from './lib/server';
+import yargs from 'yargs';
+import { asyncify } from 'asyncbox';
+
+const DEFAULT_HOST = 'localhost';
+const DEFAULT_PORT = 4797;
+
+async function main () {
+  let port = yargs.argv.port || DEFAULT_PORT;
+  let host = yargs.argv.host || DEFAULT_HOST;
+  return startServer(port, host);
+}
+
+if (require.main === module) {
+  asyncify(main);
+}
 
 export default SafariDriver;
 export { startServer };
