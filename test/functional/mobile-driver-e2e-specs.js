676ca919d2c292d68d75dd6616c5cb1be26656c6
Mykola Mokhnach <mokhnach@gmail.com>
2020-11-05 19:51:43 +0100

    Preboot simulator

--- test/functional/mobile-driver-e2e-specs.js
+++ test/functional/mobile-driver-e2e-specs.js
@@ -2,6 +2,7 @@ import wd from 'wd';
 import { startServer } from '../..';
 import chaiAsPromised from 'chai-as-promised';
 import chai from 'chai';
+import Simctl from 'node-simctl';
 
 chai.should();
 chai.use(chaiAsPromised);
@@ -9,12 +10,14 @@ chai.use(chaiAsPromised);
 const HOST = '127.0.0.1';
 const PORT = 4567;
 const MOCHA_TIMEOUT = 240000;
+const PLATFORM_VERSION = process.env.PLATFORM_VERSION || '14.1';
+const DEVICE_NAME = process.env.DEVICE_NAME || 'iPhone 11 Pro Max';
 const CAPS = {
   browserName: 'AppiumSafari',
   platformName: 'ios',
   'safari:useSimulator': true,
-  'safari:platformVersion': process.env.PLATFORM_VERSION || '14.1',
-  'safari:deviceName': process.env.DEVICE_NAME || 'iPhone 11 Pro Max',
+  'safari:platformVersion': PLATFORM_VERSION,
+  'safari:deviceName': DEVICE_NAME,
 };
 
 describe('Mobile SafariDriver', function () {
@@ -23,6 +26,18 @@ describe('Mobile SafariDriver', function () {
   let server;
   let driver;
   before(async function () {
+    // Preboot the Simulator to avoid unexpected timeouts
+    const simctl = new Simctl();
+    const allDevices = await simctl.getDevices(PLATFORM_VERSION, 'iOS');
+    const device = allDevices.find(({name}) => name === DEVICE_NAME);
+    if (!device) {
+      throw new Error(`Cannot find '${DEVICE_NAME}' Simulator (${PLATFORM_VERSION})`);
+    }
+    if (device.state !== 'Booted') {
+      simctl.udid = device.udid;
+      await simctl.bootDevice();
+      await simctl.startBootMonitor();
+    }
     server = await startServer(PORT, HOST);
   });
   after(async function () {
