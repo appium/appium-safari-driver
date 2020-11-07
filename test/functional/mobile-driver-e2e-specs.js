a3c5243bf1ff144c7dfdf66be8e949dad933f57c
Mykola Mokhnach <mokhnach@gmail.com>
2020-11-07 08:18:11 +0100

    chore: Tune the CI script (#5)

--- test/functional/mobile-driver-e2e-specs.js
+++ test/functional/mobile-driver-e2e-specs.js
@@ -2,13 +2,12 @@ import wd from 'wd';
 import { startServer } from '../..';
 import chaiAsPromised from 'chai-as-promised';
 import chai from 'chai';
+import Simctl from 'node-simctl';
+import { HOST, PORT, MOCHA_TIMEOUT } from '../utils';
 
 chai.should();
 chai.use(chaiAsPromised);
 
-const HOST = '127.0.0.1';
-const PORT = 4567;
-const MOCHA_TIMEOUT = 240000;
 const PLATFORM_VERSION = process.env.PLATFORM_VERSION || '14.1';
 const DEVICE_NAME = process.env.DEVICE_NAME || 'iPhone 11 Pro Max';
 const CAPS = {
@@ -25,11 +24,24 @@ describe('Mobile SafariDriver', function () {
   let server;
   let driver;
   before(async function () {
-    if (process.env.DEVICE_NAME) {
-      // In Azure CI the stuff fails with
+    if (process.env.CI) {
+      // In Azure CI the stuff unexpectedly fails with
       // "The device is not configured to allow remote control via WebDriver. To fix this, toggle 'Allow Remote Automation' in Safari's settings (Settings App > Safari > Advanced)."
       // error
       return this.skip();
+    }
+
+    // Preboot Simulator to avoid unexpected timeouts
+    const simctl = new Simctl();
+    const allDevices = await simctl.getDevices(PLATFORM_VERSION, 'iOS');
+    const device = allDevices.find(({name}) => name === DEVICE_NAME);
+    if (!device) {
+      throw new Error(`Cannot find '${DEVICE_NAME}' Simulator (${PLATFORM_VERSION})`);
+    }
+    if (device.state !== 'Booted') {
+      simctl.udid = device.udid;
+      await simctl.bootDevice();
+      await simctl.startBootMonitor();
     }
 
     server = await startServer(PORT, HOST);
