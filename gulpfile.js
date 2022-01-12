6b9a49efac2c8d6e209ce1260ffac55c3af21072
Mykola Mokhnach <mokhnach@gmail.com>
2022-01-12 13:50:09 +0100

    refactor!: Make the driver fully compatible with Appium 2

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 'use strict';
 
 let gulp = require('gulp');
-let boilerplate = require('appium-gulp-plugins').boilerplate.use(gulp);
+let boilerplate = require('@appium/gulp-plugins').boilerplate.use(gulp);
 
 boilerplate({
-  build: 'appium-firefox-driver',
+  build: 'appium-safari-driver',
+  files: ['index.js', 'lib/**/*.js', 'test/**/*.js', '!gulpfile.js'],
   coverage: {
     files: ['./test/unit/**/*-specs.js', '!./test/functional/**'],
     verbose: true
